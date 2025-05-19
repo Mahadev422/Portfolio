78321d075751c20e13fbf8762f6a645d205dc331
Mahadev <project9162595119@gmail.com>
2025-05-19 13:03:49 +0530

    remove three

--- src/components/main/ComputerSetup3D.jsx
+++ src/components/main/ComputerSetup3D.jsx
@@ -1,73 +1,15 @@
-import { Canvas, useFrame, useLoader, useThree } from '@react-three/fiber';
-import { Suspense, useRef, useState } from 'react';
-import { OrbitControls, Environment, Html } from '@react-three/drei';
 
-// Computer setup components with improved details
-import Desk from './computer/Desk';
-import Monitor from './computer/Monitor';
-import Keyboard from './computer/Keyboard';
-import Mouse from './computer/Mouse';
-import Chair from './computer/Chair';
-import Cpu from './computer/Cpu';
-
-
-
-// Main scene component
-const Scene = () => {
-  const groupRef = useRef();
-  const { camera } = useThree();
-  
-  // Add subtle animation
-  useFrame((state, delta) => {
-    if (groupRef.current) {
-      groupRef.current.rotation.y += delta*0.5;
-    }
-  });
-
+const EmbeddedSite = () => {
   return (
-    <>
-      <directionalLight 
-        position={[0, 0, 2]} 
-        intensity={1} 
-        castShadow
-        shadow-mapSize-width={2048}
-        shadow-mapSize-height={2048}
+    <div className="h-[100%] scale-[1.2]">
+      <iframe
+        src="https://mahadev422.github.io/computer-set/"
+        title="External Website"
+        width="100%"
+        height="100%"
       />
-      <Environment preset="city" />
-      
-      <group ref={groupRef}>
-        <Desk />
-        <Monitor />
-        <Keyboard />
-        <Mouse />
-        <Suspense>
-          <Chair modelPath="/Portfolio/models/chair.gltf" position={[0, -2, -2.2]} />
-        </Suspense>
-        <Cpu />
-      </group>
-      
-      <OrbitControls 
-        enableZoom={false}
-        enablePan={true}
-        minPolarAngle={Math.PI / 6}
-        maxPolarAngle={Math.PI / 2}
-        camera={camera}
-      />
-    </>
+    </div>
   );
 };
 
-// Main component
-const ComputerSetup3D = () => {
-  return (
-    <Canvas
-      shadows
-      camera={{ position: [0, 1, 5]}}
-      className="absolute inset-0 w-[70%]"
-    >
-      <Scene />
-    </Canvas>
-  );
-};
-
-export default ComputerSetup3D;
+export default EmbeddedSite;
