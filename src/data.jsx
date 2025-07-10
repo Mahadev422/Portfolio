93336f324723b646844b2dc93b1375487ca2b318
Mahadev <project9162595119@gmail.com>
2025-07-10 11:51:17 +0530

    baithak project

---
+++ src/data.jsx
@@ -0,0 +1,54 @@
+export const projects = [
+  {
+    title: "E-commerce Platform",
+    description:
+      "Full-featured online store with product catalog, cart, and payment integration. Built with modern technologies for optimal performance and user experience.",
+    tech: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Tailwindcss",
+      "Framer-motion",
+      "React-icons",
+    ],
+    image: "/Portfolio/project/e-commerce.png",
+    demoUrl: "https://shop-ease-git-main-mahadevs-projects-3f2fff3d.vercel.app",
+    codeUrl: "https://github.com/Mahadev422/e-commerce.git",
+  },
+  {
+    title: "Stone Paper Scissor",
+    description:
+      "Interactive game built with vanilla JavaScript featuring smooth animations and responsive design for engaging user experience.",
+    tech: ["HTML5", "CSS3", "JavaScript"],
+    image: "/Portfolio/project/Stone-paper-scissor.png",
+    demoUrl: "https://mahadev422.github.io/stone-paper-scissor/",
+    codeUrl: "https://github.com/Mahadev422/stone-paper-scissor.git",
+  },
+  {
+    title: "3D Computer Setup",
+    description:
+      "Immersive 3D visualization showcasing a complete computer workstation with interactive elements and realistic rendering.",
+    tech: ["React.js", "React-three", "Drei", "Fiber", "Tailwindcss"],
+    image: "/Portfolio/project/computer-set.png",
+    demoUrl: "https://mahadev422.github.io/computer-set/",
+    codeUrl: "https://github.com/Mahadev422/computer-set.git",
+  },
+  {
+    title: "Weather App",
+    description:
+      "Real-time weather application providing current conditions, forecasts, and location-based data with a sleek UI.",
+    tech: ["React.js", "React-Icons", "Tailwindcss"],
+    image: "/Portfolio/project/weather-app.png",
+    demoUrl: "https://mahadev422.github.io/Weather-App/",
+    codeUrl: "https://github.com/Mahadev422/Weather-App.git",
+  },
+  {
+    title: "Baithak",
+    description:
+      "Full-featured with dark mode , login with google and online store with product catalog, cart and wishlist. Built with modern technologies for optimal performance and user experience.It h",
+    tech: ["React.js", "React-Icons", "Tailwindcss", "Firebase"],
+    image: "/Portfolio/project/baithak.png",
+    demoUrl: "https://baithak-seven.vercel.app/",
+    codeUrl: "https://github.com/Mahadev422/Baithak.git",
+  },
+];
